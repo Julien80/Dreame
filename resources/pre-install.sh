54372515e1f9c2441401fd59623e27e32a9dab01
Julien80 <bailleul.j@me.com>
2023-03-12 11:03:47 +0100

    Update pre-install.sh

--- resources/pre-install.sh
+++ resources/pre-install.sh
@@ -1,38 +1,41 @@
-#!/bin/bash
+#!/bin/bash 
 
-RUST_VERSION="1.48.0"
-PYTHON_VERSION="3.8"
+RUST_VERSION="1.48.0" 
+PYTHON_VERSION="3.8" 
 
-echo "Launch pre-install of dreame dependency"
+echo "Lancement de l'installation des dépendances de Dreame"
 
-if ! python${PYTHON_VERSION:0:1}.${PYTHON_VERSION:2:1}.${PYTHON_VERSION:4:1} -c "import sys; assert sys.version_info >= (3, 8, 2)" >/dev/null 2>&1; then
-    echo "Python ${PYTHON_VERSION} or later is not installed. Installing now..."
+if ! python${PYTHON_VERSION} -c "import sys; assert sys.version_info >= (3, 8, 2)" >/dev/null 2>&1; then
+    echo "Python ${PYTHON_VERSION} ou une version ultérieure n'est pas installé. Installation en cours..."
     sudo apt-get update -q
     sudo apt-get install -y curl build-essential libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
     curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz
     tar -xf Python-${PYTHON_VERSION}.tar.xz
     cd Python-${PYTHON_VERSION}
-    echo "Running ./configure"
+    echo "Lancement de './configure'"
     ./configure --enable-optimizations
-    echo "Running make"
+    echo "Lancement de 'make'"
     make -j 4
-    echo "Running make altinstall"
+    echo "Lancement de 'make altinstall'"
     sudo make altinstall
 fi
 
-sudo apt remove -y rustc
-sudo apt remove -y cargo
-echo "Installing rustup"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
-echo "Updating rustup to version ${RUST_VERSION}"
+if ! [ -x "$(command -v rustup)" ]; then
+    echo "Installation de rustup"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+
+echo "Mise à jour de rustup vers la version ${RUST_VERSION}"
 rustup update ${RUST_VERSION}
-sudo ln -s /root/.cargo/bin/rustc /usr/bin/rustc
-sudo ln -s /root/.cargo/bin/cargo /usr/bin/cargo
-echo "Setting executable permissions for rustc and cargo"
-sudo chmod +x /usr/bin/rustc
+
+sudo ln -sf /root/.cargo/bin/rustc /usr/bin/rustc 
+sudo ln -sf /root/.cargo/bin/cargo /usr/bin/cargo 
+
+echo "Attribution des permissions d'exécution pour rustc et cargo"
+sudo chmod +x /usr/bin/rustc 
 sudo chmod +x /usr/bin/cargo
 
-echo "Installing python-miio from git master branch"
-python${PYTHON_VERSION:0:1}.${PYTHON_VERSION:2:1}.${PYTHON_VERSION:4:1} -m ensurepip
-python${PYTHON_VERSION:0:1}.${PYTHON_VERSION:2:1}.${PYTHON_VERSION:4:1} -m pip install --upgrade pip
-python${PYTHON_VERSION:0:1}.${PYTHON_VERSION:2:1}.${PYTHON_VERSION:4:1} -m pip install git+https://github.com/rytilahti/python-miio.git@master
+echo "Installation de python-miio depuis la branche master de git"
+python${PYTHON_VERSION} -m ensurepip
+python${PYTHON_VERSION} -m pip install --upgrade pip
+python${PYTHON_VERSION} -m pip install git+https://github.com/rytilahti/python-miio.git@master
