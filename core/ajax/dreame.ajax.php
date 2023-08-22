9e49e35ffb3d0b4441b7abde240d7d671d29e85d
Julien80 <bailleul.j@me.com>
2023-08-22 13:02:55 +0200

    Update dreame.ajax.php

--- core/ajax/dreame.ajax.php
+++ core/ajax/dreame.ajax.php
@@ -16,109 +16,33 @@
 */
 
 try {
-	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-	include_file('core', 'authentification', 'php');
-	
-	if (!isConnect('admin')) {
-		throw new Exception(__('401 - Accès non autorisé', __FILE__));
-	}
-	
-	require_once dirname(__FILE__) . '/../php/dreame.inc.php';
-	/* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
-	En V3 : indiquer l'argument 'true' pour contrôler le token d'accès Jeedom
-	En V4 : autoriser l'exécution d'une méthode 'action' en GET en indiquant le(s) nom(s) de(s) action(s) dans un tableau en argument
-	*/
-	ajax::init();
-	
-	
-	if (init('action') == 'detectDevices') { 
-		ajax::success(detectDevices());
-	}
-	
-	if (init('action') == 'discoverFromIp') { 
-		ajax::success(discoverFromIp());
-	}
-	
-	
-	throw new Exception(__('Aucune méthode correspondante à', __FILE__) . ' : ' . init('action'));
-	/*     * *********Catch exeption*************** */
+    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    include_file('core', 'authentification', 'php');
+    
+    if (!isConnect('admin')) {
+        throw new Exception(__('401 - Accès non autorisé', __FILE__));
+    }
+    
+    require_once dirname(__FILE__) . '/../php/dreame.inc.php';
+    /* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
+    En V3 : indiquer l'argument 'true' pour contrôler le token d'accès Jeedom
+    En V4 : autoriser l'exécution d'une méthode 'action' en GET en indiquant le(s) nom(s) de(s) action(s) dans un tableau en argument
+    */
+    ajax::init();
+    
+    
+    $dreameClass = new dreame();
+    
+    if (init('action') == 'detectDevices') { 
+        ajax::success($dreameClass->detectDevices());
+    }
+    
+    
+    throw new Exception(__('Aucune méthode correspondante à', __FILE__) . ' : ' . init('action'));
+    /*     * *********Catch exeption*************** */
 }
 
 catch (Exception $e) {
-	ajax::error(displayException($e), $e->getCode());
+    ajax::error(displayException($e), $e->getCode());
 }
 
-function detectDevices() {
-	$accountEmail = 	trim(config::byKey('account-email', 'dreame'));
-	$accountPassword = 	trim(config::byKey('account-password', 'dreame'));
-	$accountCountry = 	trim(config::byKey('account-country', 'dreame'));
-	
-	$cmd = "sudo micloud get-devices -u '" . $accountEmail . "' -p '" . $accountPassword . "' -c ". $accountCountry. " 2>&1";
-	exec($cmd,$outputArray,$resultCode);
-log::add("dreame", "debug", json_encode($outputArray));
-	if ($resultCode != 0) {
-		if (strstr( $outputArray[23],'Access denied')){ //$outputArray[23] = "micloud.micloudexception.MiCloudAccessDenied: Access denied. Did you set the correct api key and/or username?")
-			log::add("dreame", "debug", "Erreur Mot de Passe ou Email");
-
-			event::add('jeedom::alert', array(
-				'level' => 'danger',
-				'page' => 'dreame',
-				'ttl' => 10000,
-				'message' => __('Identification impossible, vérifiez votre identifiant et votre mot de passe Xiami Home.', __FILE__),
-			));
-			return ["countResults" => 0,
-			"newEq" => 0,
-			"results"=> []];
-		}
-		
-	}else{
-		$json = json_decode($outputArray[0]);
-log::add("dreame", "debug", json_encode($json));
-		$getAllDevices = eqLogic::byType('dreame');
-		foreach($json as $response) {
-			$alreadyExist = false;
-			foreach($getAllDevices as $allDevices) {
-				if ($allDevices->getLogicalId() == $response->did) {
-					$alreadyExist = true;
-					break;
-				}
-			}
-			$numberNewDevice = 0;
-
-if ($alreadyExist) {
-    log::add("dreame", "debug", "Equipement déjà présent, il ne faut donc pas l'ajouter");
-} else {
-    // Check if $response->model contains 'Dreame' or 'viomi'
-    if (strpos($response->model, 'dreame') !== false || strpos($response->model, 'viomi') !== false) {
-        $eqlogic = new dreame();
-        $eqlogic->setName($response->name);
-        $eqlogic->setIsEnable(1);
-        $eqlogic->setIsVisible(0);
-        $eqlogic->setLogicalId($response->did);
-        $eqlogic->setEqType_name('dreame');
-        $eqlogic->setConfiguration('did', $response->did);
-        $eqlogic->setConfiguration('ip', $response->localip);
-        $eqlogic->setConfiguration('token', $response->token);      
-        $eqlogic->setConfiguration('model', $response->model);
-        $eqlogic->save();
-        $numberNewDevice++;
-        log::add("dreame", "debug", "Nouvel Equipement, ajout en cours.");
-    } else {
-        log::add("dreame", "debug", "Le modèle de l'équipement n'est pas pris en charge : ".$response->model);
-    }
-}
-
-			
-		}
-		
-	}
-	
-	log::add("dreame", "debug", "============================ DISCOVER ============================");
-	
-	
-	return ["countResults" => $countResults,
-	"newEq" => $numberNewDevice,
-	"results"=> $arrayResults
-];
-
-}
